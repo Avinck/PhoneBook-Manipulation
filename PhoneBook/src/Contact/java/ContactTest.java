dab917e2d11ab9fdfd3f5e3e30f31863a9f0d67f
ck <avinash.ck@capgemini.com>
2023-01-11 19:17:38 +0530

    commited

---
+++ PhoneBook/src/Contact/java/ContactTest.java
@@ -0,0 +1,34 @@
+package Contact.java;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPhoneNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmailId() {
+    }
+
+
+}
